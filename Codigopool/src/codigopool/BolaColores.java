8f4e37b9cdff84314ab634fcdb8da7c47cef1eb4
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-12-05 22:52:00 -0300

    arreglo de bug visual

--- Codigopool/src/codigopool/BolaColores.java
+++ Codigopool/src/codigopool/BolaColores.java
@@ -1,16 +1,21 @@
 package codigopool;
-
 import java.awt.Color;
 import java.awt.Graphics;
 public class BolaColores extends Bola {
     int tipo;
     public BolaColores(double x, double y,int n){
         super(x,y,n);
-        if((getX()+getY())>=100){
+        if(n<=2){
             tipo=1;
         }
-        if((getX()+getY())<=100){
+        if(n>2 && n<=4){
             tipo=2;
+        }
+        if(n>4 && n<=6){
+            tipo=3;
+        }
+        if(n>6 && n<=8){
+            tipo=4;
         }
     }
     
@@ -21,9 +26,20 @@ public class BolaColores extends Bola {
                 case 1:
                     g.setColor(Color.red);
                     g.fillOval((int)getX()-10, (int)getY()-10,20,20);
+                    break;
                 case 2:
                     g.setColor(Color.blue);
                     g.fillOval((int)getX()-10, (int)getY()-10,20,20);
+                    break;
+                case 3:
+                    g.setColor(Color.yellow);
+                    g.fillOval((int)getX()-10, (int)getY()-10,20,20);
+                    
+                    break;
+                case 4:
+                    g.setColor(Color.orange);
+                    g.fillOval((int)getX()-10, (int)getY()-10,20,20);
+                    break;
         }
     }
             
